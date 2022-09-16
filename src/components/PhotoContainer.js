7b8bfd88dd02a529487bdae82fd7cba7c3c82459
Konstantina Roumelioti <konstantinaroumelioti@gmail.com>
2022-09-16 22:20:09 +0300

    Conditional rendering of the component if there are no data present

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -4,21 +4,34 @@ import { useParams } from 'react-router-dom';
 
 const PhotoContainer = (props) => {
   let { query } = useParams();
+  let photos=[];
+  let msg="";
     useEffect(() => {
     props.searchImg(query);
   }, [query]);
-  let photos = props.data.map((photo) => {
-    return <Photo img={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`} key={photo.id} alt={photo.title}/>
-  }); 
 
-  return (
-    <div className='photo-container'>
-    <h2>Results</h2>
-      <ul>
-        {photos}    
-      </ul>
-    </div>
-  );
+  if(props.data.length>0){
+    photos= props.data.map((photo) => {
+      return <Photo img={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`} key={photo.id} alt={photo.title}/>
+    }); 
+  }else{
+    msg="Sorry, there are no matches for your search! Why don't you try again?";
+  }
+
+  if(photos.length>0){
+    return (
+      <div className='photo-container'>
+      <h2>Results</h2>
+        <ul>
+          {photos}    
+        </ul>
+      </div>
+    );}else{
+      return(      <div className='photo-container'>
+      <h2>Results</h2>
+        {msg}
+      </div>);
+    }
 }
 
 export default PhotoContainer;
